8aca9ce6c2a8d977e88a2bd07856689430700a5e
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-07-05 22:48:16 -0400

    scroll/nav working

--- app/js/controllers/main.controller.js
+++ app/js/controllers/main.controller.js
@@ -11,7 +11,35 @@
         $('.parallax').parallax();
         }, 0);
 
+        $("#home").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#homeID").offset().top
+        }, 2000);
+        });
 
+        $("#work").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#workID").offset().top
+        }, 2000);
+        });
+
+        $("#skills").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#skillsID").offset().top
+        }, 2000);
+        });
+
+        $("#about").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#aboutID").offset().top
+        }, 2000);
+        });
+
+        $("#contact").click(function() {
+        $('html, body').animate({
+        scrollTop: $("#contactID").offset().top
+        }, 2000);
+        });
 
   }]);
 
